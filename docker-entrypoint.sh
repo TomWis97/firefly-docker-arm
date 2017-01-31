7c2e5a9fad5b2c6782b23c6de6f8dbd0f500f50b
TomWis97 <github@tomwishaupt.nl>
2017-01-31 18:09:42 +0100

    Added missing file.

---
+++ docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cp "/var/firefly/env" .env
+env | grep "^FIREFLY_CONFIG_" | sed 's/FIREFLY_CONFIG_//' >> .env
+
+migrate_status=$(php artisan migrate:status -n --env=production)
+
+if echo $migrate_status | grep -q "No migrations found";
+then
+    # The initial database setup needs to be done
+    php artisan migrate --seed --env=production --force
+else
+    if echo $migrate_status | grep -q "^\|\s+N"
+    then
+        # One or more migrations still need to be run
+        php artisan migrate --env=production --force
+    fi
+fi
+
+$@
